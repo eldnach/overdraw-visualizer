c58ffc4ee94235675353cca5a046b32b88a22076
eldnach <nachd1337@gmail.com>
2023-09-09 21:31:20 +0300

    overdraw visualizer

---
+++ Assets/Scripts/guiWidgets.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guiWidgets 
+{
+        public static float fSlider(Rect rect, float val, float minVal, float maxVal, string text, string style = "default")
+        {
+            if (style != "default") { GUI.Label(rect, text, style); } else { GUI.Label(rect, text); }
+
+            rect.y += rect.height * 1.2f;
+            val = GUI.HorizontalSlider(rect, val, minVal, maxVal);
+
+            return val;
+        }
+
+        public static int iSlider(Rect rect, int val, int minVal, int maxVal, string text, string style = "default")
+        {
+            if (style != "default") { GUI.Label(rect, text, style); } else { GUI.Label(rect, text); }
+
+            rect.y += rect.height * 1.2f;
+            val = (int)GUI.HorizontalSlider(rect, val, minVal, maxVal);
+            return val;
+        }
+
+        public static Vector3 rgbSlider(Rect rect, Vector3 val, string text, string style = "default")
+        {
+            float r = val.x;
+            float g = val.y;
+            float b = val.z;
+
+            if (style != "default") { GUI.Label(rect, text, style); } else { GUI.Label(rect, text); }
+
+            rect.y += rect.height * 1.2f;
+            r = GUI.HorizontalSlider(rect, r, 0.0f, 1.0f);
+            rect.y += rect.height * .8f;
+            g = GUI.HorizontalSlider(rect, g, 0.0f, 1.0f);
+            rect.y += rect.height * .8f;
+            b = GUI.HorizontalSlider(rect, b, 0.0f, 1.0f);
+
+            return new Vector3(r, g, b);
+        }
+
+        public static Vector3 vec3Slider(Rect rect, Vector3 val, Vector2 xRange, Vector2 yRange, Vector2 zRange, string text, string style = "default")
+        {
+            float r = val.x;
+            float g = val.y;
+            float b = val.z;
+
+            if (style != "default") { GUI.Label(rect, text, style); } else { GUI.Label(rect, text); }
+
+            rect.y += rect.height * 1.0f;
+            r = GUI.HorizontalSlider(rect, r, xRange.x, xRange.y); 
+            rect.y += rect.height;
+            g = GUI.HorizontalSlider(rect, g, yRange.x, yRange.y); 
+            rect.y += rect.height;
+            b = GUI.HorizontalSlider(rect, b, zRange.x, zRange.y); 
+
+            return new Vector3(r, g, b);
+        }
+}
---
+++ Assets/Scripts/overdrawViz.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overdrawViz : MonoBehaviour
+{
+    public Shader shader;
+
+    Camera cam;
+
+    int amountX;
+    int amountY;
+
+    ComputeShader computeShader;
+    ComputeBuffer buffer0;
+    RenderTexture overdrawRT;
+
+    public int minRange;
+    public int maxRange;
+
+    int[] counter;
+
+    int average;
+    int max;
+
+    public GUISkin guiSkin;
+    public bool enableGUI;
+
+    struct ComputeKernel
+    {
+        public int[] workGroupSize;
+        public int[] workGroupAmount;
+        public int kernelID;
+    }
+
+    ComputeKernel computeKernel;
+    ComputeKernel computeKernelTriangles;
+
+    void InitializeComputeBuffers()
+    {
+        counter = new int[amountX * amountY];
+
+        for (int i = 0; i < amountX; i++)
+        {
+            for (int j = 0; j < amountY; j++)
+            {
+                counter[j * amountX + i] = 0;
+            }
+        }
+
+        int stride = System.Runtime.InteropServices.Marshal.SizeOf(typeof(int));
+        buffer0 = new ComputeBuffer(amountX * amountY, stride, ComputeBufferType.Default);
+        buffer0.SetData(counter);
+    }
+
+    void DownloadData()
+    {
+        buffer0.GetData(counter);
+        int sum = 0;
+        for (int i = 0; i < amountX; i++)
+        {
+            for (int j = 0; j < amountY; j++)
+            {
+               sum += counter[j * amountX + i];
+            }
+        }
+        average = sum / (amountY * amountY);
+        Debug.Log("Average overdraw count per pixel " + average);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        amountX = Screen.width;
+        amountY = Screen.height;
+        max = 0;
+        average = 0;
+
+        overdrawRT = new RenderTexture(amountX, amountY, 24);
+        overdrawRT.enableRandomWrite = true;
+        overdrawRT.Create();
+
+        computeShader = Resources.Load<ComputeShader>("ComputeOverdraw");
+        computeKernel = new ComputeKernel();
+        computeKernel.workGroupSize = new int[3] { 8, 8, 1 };
+        computeKernel.workGroupAmount = new int[3]
+        {
+            Mathf.CeilToInt(amountX / computeKernel.workGroupSize[0]),
+            Mathf.CeilToInt(amountY / computeKernel.workGroupSize[1]),
+            1
+        };
+        computeKernel.kernelID = computeShader.FindKernel("Overdraw");
+
+        InitializeComputeBuffers();
+
+        // Before drawing, replace all shaders with "overdraw" shader
+        cam = gameObject.GetComponent<Camera>();
+        cam.SetReplacementShader(shader, "RenderType");
+
+        //camera.SetReplacementShader(transparentShader, "opaqueTag");
+
+    }
+
+    void OnDestroy()
+    {
+        buffer0.Dispose();
+    }
+
+
+    // 1. After rendering to overdraw texture to framebuffer, use framebuffer as input for compute shader
+    // 2. Compute shader calculates "overdraw count" from overdraw texture, and outputs to a color target (overdrawRT)
+    // 3. overdrawRT gets blit to framebuffer
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+
+        computeShader.SetTexture(computeKernel.kernelID, "_overdrawMap", src, 0, UnityEngine.Rendering.RenderTextureSubElement.Color);
+        computeShader.SetTexture(computeKernel.kernelID, "_colorMap", overdrawRT, 0, UnityEngine.Rendering.RenderTextureSubElement.Color);
+        counter[0] = 0;
+        buffer0.SetData(counter);
+        computeShader.SetBuffer(computeKernel.kernelID, "_counter", buffer0);
+
+        computeShader.SetInt( "_rangeMin", minRange);
+        computeShader.SetInt( "_rangeMax", maxRange);
+        computeShader.SetInt("_totalX", amountX);
+        computeShader.SetInt("_totalY", amountY);
+        computeShader.Dispatch(computeKernel.kernelID, computeKernel.workGroupAmount[0], computeKernel.workGroupAmount[1], computeKernel.workGroupAmount[2]);
+
+        Graphics.Blit(overdrawRT, dest);
+
+
+    }
+
+    void OnGUI()
+    {
+        if (enableGUI)
+        {
+            float xmargin = 30.0f;
+            float ymargin = 30.0f;
+
+            int width = amountX / 8;
+            int height = amountY / 25;
+            GUI.skin = guiSkin;
+            Rect rect = new Rect(xmargin, ymargin, width, height);
+
+            rect = new Rect(xmargin, ymargin - 20.0f, width, height * 10.0f);
+            GUI.Label(rect, "Overdraw Visualizer");
+
+            rect = new Rect(xmargin, ymargin + height * 0.0f, width, height * .5f);
+            minRange = guiWidgets.iSlider(rect, minRange, 0, 10, "Minimum overdraw range (" + minRange.ToString() + ")");
+
+            rect = new Rect(xmargin, ymargin + height * 1.0f, width, height * .5f);
+            maxRange = guiWidgets.iSlider(rect, maxRange, 0, 10, "Maximum overdraw range (" + maxRange.ToString() + ")");
+
+            rect = new Rect(xmargin, ymargin + height * 2.25f, width, height * .5f);
+            if (GUI.Button(rect, "Download overdraw counters"))
+            {
+                DownloadData();
+            }
+            rect = new Rect(xmargin, ymargin + height * 3.0f, width, height * .5f);
+            GUI.Label(rect, "Average overdraw count: " + average.ToString());
+
+            rect = new Rect(xmargin, ymargin + height * 3.5f, width, height * .5f);
+            GUI.Label(rect, "Maximum overdraw count: " + max.ToString());
+
+
+        }
+
+    }
+
+}
